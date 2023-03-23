7c84ceba0d4a9b614005ff6af3c48612f0340717
sharahgallego <128505493+sharahgallego@users.noreply.github.com>
2023-03-23 13:51:33 -0600

    autocomplete of form 'off'

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ function displayWeatherCondition(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
