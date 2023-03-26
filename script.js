d80a5669e7fbcc369d3f5ae9133b2b5b863e5d86
sharahgallego <128505493+sharahgallego@users.noreply.github.com>
2023-03-26 16:47:55 -0600

    Added a list of days to forecast

--- script.js
+++ script.js
@@ -22,10 +22,30 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2 weekday"> <div class="weather-forecast-date">${day}</div><img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                /><div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-min">  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span> </span></div></div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeatherCondition(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
+  displayForecast();
   document.querySelector("#temperature").innerHTML =
     Math.round(celsiusTemperature) + "°C";
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
